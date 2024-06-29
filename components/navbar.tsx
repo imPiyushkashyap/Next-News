dc84c918ce744a7f4d656f2afeffec97530e8780
Piyushkashya <piyush8580kashyap@gmail.com>
2024-06-29 17:08:37 +0530

    feat: slidebar is initialize

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -53,24 +53,14 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
+            {/* <Logo /> */}
+            {/* logo */}
+            <p className="font-bold text-inherit"></p>
           </NextLink>
         </NavbarBrand>
         <ul className="hidden lg:flex gap-4 justify-start ml-2">
           {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium"
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
+            <NavbarItem key={item.href}></NavbarItem>
           ))}
         </ul>
       </NavbarContent>
